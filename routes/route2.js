c9506af90ced447614aa80d46733d909ac88c415
Maria Kalusz <mkalusz1@gmail.com>
2019-03-02 21:43:50 -0800

    more fixings

--- routes/route2.js
+++ routes/route2.js
@@ -2,22 +2,24 @@ var express = require('express');
 var router = express.Router();
 var colorObject = require( '../colors.json');
 
-function getColorByName(colorString) {
-    for(var i = 0; i < colorObject.colors.length; i++) {
-        if(colorObject.colors[i].color === colorString) {
-            return colorObject.colors[i];
-        }
-    } 
-}
-
 /* GET route2 listing. */
 router.get('/all', function(req, res, next) {
     res.send(colorObject);
 });
 
-router.get('/:colors', function(req, res, next) {
+router.get('/:color', function(req, res, next) {
     var myColor = getColorByName(req.params.color);
     res.send(myColor);
 });
+
+function getColorByName(colorString) {
+    for(var i = 0; i < colorObject.colors.length; i++) {
+        if(colorObject.colors[i].color === colorString) {
+            return colorObject.colors[i];
+        } else {
+            return null;
+        }
+    } 
+}
 
 module.exports = router;
