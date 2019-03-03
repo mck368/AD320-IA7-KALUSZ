f448b07d5331fdac2aeef7da2a57cf70d7f62118
Maria Kalusz <mkalusz1@gmail.com>
2019-03-02 17:10:48 -0800

    fixing

--- routes/route2.js
+++ routes/route2.js
@@ -3,12 +3,39 @@ var router = express.Router();
 var fs = require('fs');
 var color = require( '../colors.json');
 
-var contents = fs.readFileSync("./colors.json");
-var jsonContent = JSON.parse(contents);
-
 /* GET route2 listing. */
-router.get("/:colors", function(req, res, next) {
-    res.json(jsonContent.colors[0]);
+router.get('/all', function(req, res, next) {
+    res.send(color);
+});
+
+router.get('/black', function(req, res, next) {
+    var black = color.colors[0];
+    res.send(black);
+});
+
+router.get('/white', function(req, res, next) {
+    var white = color.colors[1];
+    res.send(white);
+});
+
+router.get('/red', function(req, res, next) {
+    var red = color.colors[2];
+    res.send(red);
+});
+
+router.get('/blue', function(req, res, next) {
+    var blue = color.colors[3];
+    res.send(blue);
+});
+
+router.get('/yellow', function(req, res, next) {
+    var yellow = color.colors[4];
+    res.send(yellow);
+});
+
+router.get('/green', function(req, res, next) {
+    var green = color.colors[5];
+    res.send(green);
 });
 
 module.exports = router;
