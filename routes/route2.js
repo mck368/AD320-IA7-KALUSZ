816c65bb6add0456f606e68443781c691c4a9c8b
Maria Kalusz <mkalusz1@gmail.com>
2019-03-02 21:38:29 -0800

    more fixings

--- routes/route2.js
+++ routes/route2.js
@@ -1,41 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
-var color = require( '../colors.json');
+var colorObject = require( '../colors.json');
+
+function getColorByName(colorString) {
+    for(var i = 0; i < colorObject.colors.length; i++) {
+        if(colorObject.colors[i].color === colorString) {
+            return colorObject.colors[i];
+        }
+    } 
+}
 
 /* GET route2 listing. */
 router.get('/all', function(req, res, next) {
-    res.send(color);
+    res.send(colorObject);
 });
 
-router.get('/black', function(req, res, next) {
-    var black = color.colors[0];
-    res.send(black);
-});
-
-router.get('/white', function(req, res, next) {
-    var white = color.colors[1];
-    res.send(white);
-});
-
-router.get('/red', function(req, res, next) {
-    var red = color.colors[2];
-    res.send(red);
-});
-
-router.get('/blue', function(req, res, next) {
-    var blue = color.colors[3];
-    res.send(blue);
-});
-
-router.get('/yellow', function(req, res, next) {
-    var yellow = color.colors[4];
-    res.send(yellow);
-});
-
-router.get('/green', function(req, res, next) {
-    var green = color.colors[5];
-    res.send(green);
+router.get('/:colors', function(req, res, next) {
+    var myColor = getColorByName(req.params.color);
+    res.send(myColor);
 });
 
 module.exports = router;
